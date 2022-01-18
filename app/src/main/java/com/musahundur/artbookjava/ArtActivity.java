63fd94bb26b60c4569e06f6d7723882d27641658
Musa <musaabdullah.hundur@gmail.com>
2022-01-18 17:07:26 +0300

    update

---
+++ app/src/main/java/com/musahundur/artbookjava/ArtActivity.java
@@ -0,0 +1,14 @@
+package com.musahundur.artbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+}
--- app/src/main/java/com/musahundur/artbookjava/MainActivity.java
+++ app/src/main/java/com/musahundur/artbookjava/MainActivity.java
@@ -3,12 +3,28 @@ package com.musahundur.artbookjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.musahundur.artbookjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
+
+    public void save(View view){
+
+    }
+
+    public void selectImage(View view){
+
+    }
+
 }
