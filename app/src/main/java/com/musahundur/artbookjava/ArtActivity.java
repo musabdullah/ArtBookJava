e69d8fcf1776aaa1e2797d79cb7612f57caa6711
Musa <musaabdullah.hundur@gmail.com>
2022-01-25 12:04:50 +0300

    update

--- app/src/main/java/com/musahundur/artbookjava/ArtActivity.java
+++ app/src/main/java/com/musahundur/artbookjava/ArtActivity.java
@@ -12,6 +12,8 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.graphics.Bitmap;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
@@ -25,12 +27,15 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.musahundur.artbookjava.databinding.ActivityArtBinding;
 
+import java.io.ByteArrayOutputStream;
+
 public class ArtActivity extends AppCompatActivity {
 
     private ActivityArtBinding binding;
     ActivityResultLauncher<Intent> activityResultLauncher;
     ActivityResultLauncher<String> permissionLauncher;
     Bitmap selectedImage;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +46,76 @@ public class ArtActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        registerLauncher();
+    }
+
+    public Bitmap makeSmallerImage(Bitmap image, int maksimumSize){
+        int width = image.getWidth();
+        int height= image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height ;
+
+        if (bitmapRatio>1){
+            //landscape image
+            width = maksimumSize;
+            height = (int) (width / bitmapRatio);
+        }else{
+            //portrait image
+            height = maksimumSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return image.createScaledBitmap(image,width,height,true);
     }
 
     public void save(View view){
+        String name = binding.editTextName.getText().toString();
+        String artistName = binding.editTextArtist.getText().toString();
+        String year = binding.editTextYear.getText().toString();
 
+        Bitmap smallImage = makeSmallerImage(selectedImage,300);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        smallImage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[] byteArray = outputStream.toByteArray();
+
+        try {
+            database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts (id INTEGER PRIMARY KEY, artname VARCHAR, paintername VARCHAR,year VARCHAR, image BLOB)");
+
+            String sqlString = "INSERT INTO arts (artname, paintername, year, image) VALUES (?,?,?,?)";
+            SQLiteStatement sqLiteStatement = database.compileStatement(sqlString);
+            sqLiteStatement.bindString(1,name);
+            sqLiteStatement.bindString(2,artistName);
+            sqLiteStatement.bindString(3, year);
+            sqLiteStatement.bindBlob(4,byteArray);
+            sqLiteStatement.execute();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(ArtActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 
     public void selectImage(View view){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.MANAGE_EXTERNAL_STORAGE)){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
                 Snackbar.make(view, "Permission needed for gallery", BaseTransientBottomBar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         //request permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
                     }
                 }).show();
             }else{
                 //request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
             }
         }else{
             //gallery
             Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
         }
     }
 
@@ -97,6 +151,7 @@ public class ArtActivity extends AppCompatActivity {
                 if (result){
                     //permission granted
                     Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
                 }else{
                     //permission denied
                     Toast.makeText(ArtActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
@@ -104,5 +159,4 @@ public class ArtActivity extends AppCompatActivity {
             }
         });
     }
-
 }
